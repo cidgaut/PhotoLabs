ff4e31d9f96b662206cecdf96d97c5b697b0ef5d
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 02:49:20 -0500

    removed sample data and moved it into a seperate array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,47 +3,15 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem1 = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const sampleDataForPhotoListItem2 = {
-  id: "2",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const sampleDataForPhotoListItem3 = {
-  id: "3",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const photos = [sampleDataForPhotoListItem1, sampleDataForPhotoListItem2, sampleDataForPhotoListItem3];
   return (
     <div className="App">
-      {photos.map((photo, index) =>
+      {/*photos.map((photo, index) =>
       <PhotoListItem key={index} photo={photo}/>
-      )}
+  )*/}
     </div>
   );
 };

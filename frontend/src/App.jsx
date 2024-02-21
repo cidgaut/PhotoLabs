2176777d75163a08e4ca3eb3bd944e665959a659
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 01:14:08 -0500

    put photos into an array and used keys to display images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,30 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
+const sampleDataForPhotoListItem1 = {
   id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const sampleDataForPhotoListItem2 = {
+  id: "2",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const sampleDataForPhotoListItem3 = {
+  id: "3",
   location: {
     city: "Montreal",
     country: "Canada",
@@ -16,9 +38,12 @@ const sampleDataForPhotoListItem = {
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photos = [sampleDataForPhotoListItem1, sampleDataForPhotoListItem2, sampleDataForPhotoListItem3];
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem}/>
+      {photos.map((photo, index) =>
+      <PhotoListItem key={index} photo={photo}/>
+      )}
     </div>
   );
 };

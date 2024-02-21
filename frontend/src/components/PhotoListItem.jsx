d48440919f462d762a9e1850a52fe8d08cb67228
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 00:27:53 -0500

    parsed data into PhotoListItem to be used to render sample content

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,18 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <article className="photo-list-item">
+    <img src={imageSource} alt={`Photo by ${username}`} className="photo-img" />
+      <div className="photo-details">
+        <img src={profile} alt={`Profile of ${username}`} className="profile-img" />
+        <p className="photo-location">{location.city}, {location.country}</p>
+        <p className="photo-username">{username}</p>
+      </div>
+    </article>
+ );
 };
 
 export default PhotoListItem;

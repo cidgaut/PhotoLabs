7c6f7264e7314dc05104688e66e82bcf9f46abc6
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 02:03:58 -0500

    removed callback function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,11 +1,9 @@
-import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
   const [favouritePhoto, setFavouritePhoto] = useState(false);
-  
+
   return (
     <div className="photo-list__fav-icon" onClick={() => setFavouritePhoto(prev => !prev)}>
       <div className="photo-list__fav-icon-svg">

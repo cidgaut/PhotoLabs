f7f788669842ca0d36dd5e73e41861df997faed4
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 02:53:21 -0500

    imported PhotoList into App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = [sampleDataForPhotoListItem1, sampleDataForPhotoListItem2, sampleDataForPhotoListItem3];
   return (
     <div className="App">
-      {/*photos.map((photo, index) =>
-      <PhotoListItem key={index} photo={photo}/>
-  )*/}
+      <PhotoList />
     </div>
   );
 };

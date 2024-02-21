9b48d2c42a8e6a2a4a9afdd21ca0245cd8af1517
Cedric.gauthier <cidgaut@gmail.com>
2024-02-21 00:59:15 -0500

    added styling to the PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,12 +7,12 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
   return (
-    <article className="photo-list-item">
-    <img src={imageSource} alt={`Photo by ${username}`} className="photo-img" />
-      <div className="photo-details">
-        <img src={profile} alt={`Profile of ${username}`} className="profile-img" />
-        <p className="photo-location">{location.city}, {location.country}</p>
-        <p className="photo-username">{username}</p>
+    <article className="photo-list__item">
+    <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image" />
+      <div className="photo-list__user-profile">
+        <img src={profile} alt={`Profile of ${username}`} className="photo-list__user-profile" />
+        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+        <p className="photo-list__user-info">{username}</p>
       </div>
     </article>
  );

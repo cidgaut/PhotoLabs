277f30588feafd2da2d1ea5805c8f3617be864c6
Cedric.gauthier <cidgaut@gmail.com>
2024-02-22 22:58:23 -0500

    will need to revert to previous

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import './App.scss';
 
 //components
 import HomeRoute from 'routes/HomeRoute';
+import topics from "./mocks/topics";
+import photos from "./mocks/photos";
 
 
 // Note: Rendering a single component to build components in isolation
@@ -15,7 +17,7 @@ const App = () => {
       {/*photos.map((photo, index) =>
       <PhotoListItem key={index} photo={photo}/>
   )*/}
-      <HomeRoute />
+      <HomeRoute topics={topics} photos={photos}/>
     </div>
   );
 };

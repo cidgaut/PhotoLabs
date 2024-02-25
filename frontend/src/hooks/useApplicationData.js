69206cd76367a7d89e4eb9e13d59cb9cc0ad74fb
Cedric.gauthier <cidgaut@gmail.com>
2024-02-25 06:14:19 -0500

    added useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,31 +1,47 @@
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
-//initial states
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {
+        ...state,
+        favoritePhotos: state.favoritePhotos.includes(action.payload.id)
+          ? state.favoritePhotos.filter((id) => id !== action.payload.id)
+          : [...state.favoritePhotos, action.payload.id],
+      };
+
+    default:
+      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+  }
+}
+
 const useApplicationData = () => {
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
     photos: [],
-    favoritePhotos: [], 
-    selectedPhoto: null, 
-    modalOpen: false, 
+    favoritePhotos: [],
+    selectedPhoto: null,
+    modalOpen: false,
   });
 
-  
-
   const updateToFavPhotoIds = (photoId) => {
-    setState((prev) => ({
-      ...prev,
-      favoritePhotos: prev.favoritePhotos.includes(photoId)
-        ? prev.favoritePhotos.filter((id) => id !== photoId)
-        : [...prev.favoritePhotos, photoId],
-    }));
+    dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { id: photoId } });
   };
 
   const setPhotoSelected = (photo) => {
-    setState((prev) => ({ ...prev, selectedPhoto: photo }));
+    dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { photo } });
   };
 
   const onClosePhotoDetailsModal = () => {
-    setState((prev) => ({ ...prev, modalOpen: false }));
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { modalOpen: false } });
   };
 
   
